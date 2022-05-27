ae147897e510c92f381fdedf67a1f55e845e73c1
nwjbrandon <nwjbrandon@gmail.com>
2022-05-28 02:05:15 +0800

    feat: move text to ts file

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import ChatBot from 'components/ChatBot';
 import GithubIcon from 'assets/images/github.svg';
 import GmailIcon from 'assets/images/gmail.svg';
 import LinkedinIcon from 'assets/images/linkedin.png';
+import Portfolio from 'utils/Portfolio';
 import './App.scss';
 
 const App: React.FC = () => {
@@ -26,59 +27,16 @@ const App: React.FC = () => {
           <Introduction />
         </div>
         <div className="app__background">
-          I am a final year undergraduate at the National University of
-          Singapore (NUS). I am pursuing a degree in Computer Engineering with a
-          specialization in Robotics. My interests are in AI, Robotics, and IoT.
-          I was a teacher assistant for CG4002 Teaching Asistant AY2021/2022 
-          Semester 2, CG4002 Teaching Assistant AY2021/2022 Semester 1, and
-          CS1010 Teaching Assistant AY2019/2020 Semester 1. I am also a NUS Merit 
-          Scholarship holder.
+          {Portfolio.Introduction.en}
         </div>
         <div className="app__work">
           <WallpaperText text="Experiences" />
         </div>
         <div className="app__background">
           <Slider {...settings}>
-          <SlickCard
-              header={
-                'Alglorithm Engineer Intern (NLP) at Shopee\nDec 2021 - Present'
-              }
-              body={
-                'TBD'
-              }
-            />
-          <SlickCard
-              header={
-                'AI Developer Intern at Augmentus\nMay 2021 - Sep 2021'
-              }
-              body={
-                'Optimized inference speed of machine learning models on Jetson platform to run 3 times faster\nDeveloped concurrent modules to control robot, run model inference, and stream images to device via TCP\nFinetuned hyper-parameters and data augmentation steps to improve models inference on smaller dataset\nIntegrated model training with AWS to automate the training process with 70% cost savings'
-              }
-            />
-            <SlickCard
-              header={
-                'System Engineer (IoT) Intern at GovTech\nDec 2020 - March 2021'
-              }
-              body={
-                'Researched and evaluated different state-of-art deep learning models to estimate crowd count\nCreated training dataset from video footage on taget environment to finetune machine learning models\nIntegrated machine learning model with Docker on Jetson Platform to operate in the server room'
-              }
-            />
-            <SlickCard
-              header={
-                'Data Analytics/Natural Language Processing Intern at GovTech\nJan 2020 - Jul 2020'
-              }
-              body={
-                'Build and analyze NLP models to improve chatbot dataset from multiple government agencies\nDeveloped dashboard to visualize analysis from NLP models and integrate with Google DialogFlow APIs\nCreated scripts to automate deployment of dashboard using Docker and Kubernetes on AWS'
-              }
-            />
-            <SlickCard
-              header={
-                'Software Developer Intern at Hodlnaut\nMay 2019 - Jul 2019'
-              }
-              body={
-                'Developed APIs and improved user interface for users to deposit and invest in cryptocurrencies\nImplemented end to end testing and integrated into deployment pipeline for continuous testing'
-              }
-            />
+            {
+              Portfolio.Experiences.map((exp, i) => <SlickCard key={i} header={exp.en.title} body={exp.en.description} />)
+            }
           </Slider>
         </div>
         <div className="app__projects">
@@ -86,24 +44,9 @@ const App: React.FC = () => {
         </div>
         <div className="app__background">
           <Slider {...settings}>
-            <SlickCard
-                header={'CG4001 B. Eng. Dissertation (Teleoperate robotic arm via 3D pose estimation)\nAug 2021 - Present'}
-                body={
-                  'Trained Graph Convolutional Network models in PyTorch for 3D pose estimation of upper body\nCreated robot simulation with Unity Engine to teleoperate a robotic arm with the 3D pose information'
-                }
-              />
-              <SlickCard
-                header={'CG4002 Computer Engineering Capstone Project (Create wearable devices to classify dance moves)\nJan 2021 - May 2021'}
-                body={
-                  'Performed time and frequency domain feature engineering on time series IMU data in pre-processing steps\nTrained machine learning model in PyTorch on extracted features to classify dance moves with 97% accuracy'
-                }
-              />
-              <SlickCard
-                header={'Sixth Sense (Increase digital presence of Bengkala for experiential tourism)\nJan 2019 - Aug 2019'}
-                body={
-                  'Led a group of 11 students for overseas community service project to promote experiential tourism in Bengkala\nOrganized and coordinated project plans, finances, and logistics with a student group in Indonesia\nDeveloped website and deployed on DigitalOcean for the community to update and share stories of themselves'
-                }
-              />
+          {
+              Portfolio.Projects.map((proj, i) => <SlickCard key={i} header={proj.en.title} body={proj.en.description} />)
+            }
           </Slider>
         </div>
         <div className="app__awards">
@@ -111,40 +54,10 @@ const App: React.FC = () => {
         </div>
         <div className="app__background">
           <Slider {...settings}>
-            <SlickCard
-              header={'Commendation Letter For CG4002 \nDec 2021'}
-              body={'NUS'}
-            />
-            <SlickCard
-              header={'Foundations In AI\nAug 2020'}
-              body={
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://drive.google.com/file/d/1LlnFH7QGVnayCvQh_2vxyh7YRqCbmR__/view"
-                >
-                  AI Singapore
-                </a>
-              }
-            />
-            <SlickCard
-              header={
-                'Youth Expedition Project Leaders Training Course YMCA Of Singapore\nJan 2019'
-              }
-              body={
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://drive.google.com/file/d/12Bkge9pS6dQqJzQ-z-bMyRr6Gi-c9mDG/view"
-                >
-                  Youth Corps Singapore
-                </a>
-              }
-            />
-            <SlickCard
-              header={'NUS Merit Scholarship \nJul 2018'}
-              body={'NUS'}
-            />
+          {
+              Portfolio.Awards.map((proj, i) => <SlickCard key={i} header={proj.en.title} body={proj.en.description} />)
+            }
+
           </Slider>
         </div>
         <div className="app__contact">
